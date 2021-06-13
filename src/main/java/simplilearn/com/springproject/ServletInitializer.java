2cea395b612dd5579af28733e10f9b52212fbf30
Marwa GHZAIEL <mgzaiel@gmail.com>
2021-06-13 19:08:09 +0200

    Update ServletInitializer.java

--- src/main/java/simplilearn/com/springproject/ServletInitializer.java
+++ src/main/java/simplilearn/com/springproject/ServletInitializer.java
@@ -2,12 +2,12 @@ package simplilearn.com.springproject;
  
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
- 
+
 public class ServletInitializer extends SpringBootServletInitializer {
- 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringBootTomcatApplication.class);
-    }
- 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
 }
